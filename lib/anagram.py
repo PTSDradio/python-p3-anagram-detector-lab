13969bcf439426ad029157cab22db0d02aabab8e
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-07-27 10:16:16 -0500

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_of_words):
+        anagram_list = []
+        for item in list_of_words:
+            if len(self.word) == len(item) and sorted(self.word) == sorted(item):
+                anagram_list.append(item)
+
+        return anagram_list
+
+        pass
